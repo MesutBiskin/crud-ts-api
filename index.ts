e2171a45e9275fb139c03ec6688461f41dcbac1f
Mesut Biskin <msbiskin@gmail.com>
2023-11-13 17:03:22 -0500

    updated some errors

--- index.ts
+++ index.ts
@@ -1,82 +1,67 @@
-//  TYPESCRIPT TE API VE MONGO DATABASE CONNECTION KURULUMU.
+// 1. import packages
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { MongoClient, ObjectId } from 'mongodb'
+import bcrypt from 'bcrypt'
 
-
-// 1- import packages
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import { MongoClient, ObjectId } from "mongodb";
-import bcrypt from 'bcrypt';
-
-// 2-use packages.
-const app = express()
-app.use(cors())
-app.use(express.json());
-
-const client = new MongoClient(process.env.MONGO_URI as string);
+const client = new MongoClient(process.env.MONGO_URI as string)
 const db = client.db('dinos-store')
 const users = db.collection('users')
 
-client.connect();
+client.connect()
 
-//console.log("Connected to Mongo");
+// 2. use packages
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-// 3-Listen on port
-app.listen(process.env.PORT, () => console.log('Server is running on port 8080'));
+// 3. listen on port
+app.listen(process.env.PORT, () => console.log('api running here 😎'))
 
-
-//  4-create  a get endpoint
+// 4. create a get endpoint
 app.get('/', async (req, res) => {
-    const allUsers = await users.find({}).toArray()
-    res.status(201).send(allUsers)
-});
-// 5- CREATE USERS ENDPOINT
+	const allUsers = await users.find().toArray()
+	res.send(allUsers)
+})
+
+// 5. create endpoint to add users
 app.post('/', async (req, res) => {
-    const userEmail = req.body.email
-    const userPassword = req.body.password
+	const userEmail = req.body.email
+	const userPassword = req.body.password
 
-    const hashedPassword = await bcrypt.hash(userPassword, 10);
-    const userAdded = await users.insertOne({ email: userEmail, password: hashedPassword });
-    res.status(201).send(userAdded)
-
+	const hashPass = await bcrypt.hash(userPassword, 10)
+	const userAdded = await users.insertOne({ email: userEmail, password: hashPass })
+	res.status(201).send(userAdded)
 })
 
-// 6- create a delete endpoint by email with params
+// 6. create delete endpoint by _id with params
 app.delete('/:_id', async (req, res) => {
-    const cleanId = new ObjectId(req.params._id)
-    console.log(req.params)
-    const userDeleted = await users.findOneAndDelete({ _id: cleanId })
-    res.send(userDeleted)
+	const cleanId = new ObjectId(req.params._id)
+	console.log('req.params ->', req.params) // { _id: '9879769879698897987' }
+	const userDeleted = await users.findOneAndDelete({ _id: cleanId })
+	res.send(userDeleted)
 })
-// 7- create a patch endpoint by _id with params
 
+// 7. create a patch endpoint by _id with params
 app.patch('/:_id', async (req, res) => {
-    const cleanId = new ObjectId(req.params._id)
-    const userUpdated = await users.findOneAndUpdate({ _id: cleanId }, { $set: req.body })
-    res.send(userUpdated)
+	const limpoId = new ObjectId(req.params._id)
+	const itemUpdated = await users.findOneAndUpdate({ _id: limpoId }, { $set: req.body })
+	res.send(itemUpdated)
 })
 
-// 8- Log in endpoint
+// 8. login endpoint
 app.post('/login', async (req, res) => {
-    const userPasword = req.body.password
-    const foundUser = await users.findOne({ email: req.body.email })
+	const userPassword = req.body.password
+	const foundUser = await users.findOne({ email: req.body.email })
 
-    if (foundUser) {
-        const passInDb = foundUser?.password
-        const result = bcrypt.compare(userPasword, passInDb)
-        console.log(result)
+	if (foundUser) {
+		const passInDb = foundUser?.password
+		const result = await bcrypt.compare(userPassword, passInDb)
+		console.log('result -> ', result) // true / false
 
-        res.send(foundUser)
-    } else {
-        res.json("user not found!!!!")
-    }
+		res.send(foundUser)
+	} else {
+    res.json('User not found!!! ❌')
+  }
 })
-
-
-
-
-
-
-
-
-
